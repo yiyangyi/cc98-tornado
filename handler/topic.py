0f3815ed22c4e25d311f36e0d9be9c5b38bd32bd
yiyangyi <yiyangyigg@gmail.com>
2015-03-16 13:27:34 +0800

    Create the basic structure for the topic handler.

---
+++ handler/topic.py
@@ -0,0 +1,15 @@
+class IndexHandler(BaseHandler):
+
+class ViewHandler(BaseHandler):
+
+class CreateHandler(BaseHandler):
+
+class EditHandler(BaseHandler):
+
+class FavoriteHandler(BaseHandler):
+
+class CancelFavoriteHandler(BaseHandler):
+
+class VoteHandler(BaseHandler):
+
+class ReplyEditHandler(BaseHandler):
