f00a19648d5bcf8c6ba95daaec70101118c76aed
yiyangyi <yiyangyigg@gmail.com>
2015-03-16 13:17:55 +0800

    Defined the basic structure for the user handler.

---
+++ handler/user.py
@@ -0,0 +1,31 @@
+import re
+import time
+import json
+import uuid
+import Image
+import urllib
+import urllib2
+import StringIO
+import tornado.web
+
+def login(self, user_id):
+
+def logout(self):
+
+class RegisterHandler(BaseHandler):
+
+class LoginHandler(BaseHandler):
+
+class logoutHandler(BaseHandler):
+
+class SettingPasswordHandler(BaseHandler):
+
+class ForgotPasswordHandler(BaseHandler):
+
+class SettingHandler(BaseHandler):
+
+class SettingAvatarHandler(BaseHandler):
+
+class SettingAvatarFromGravatarHandler(BaseHandler):
+
+class HomeHandler(BaseHandler):
